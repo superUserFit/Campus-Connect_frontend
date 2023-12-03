2f954a1f58467d2193a07a3e79d38e13b04fc6f6
NightOwl <102517523+superUserFit@users.noreply.github.com>
2023-12-03 21:43:00 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		port: 3000,
-		// Get rid of the CORS error
-		proxy: {
-			"/api": {
-				target: "https://campus-connect-backend-t60k.onrender.com",
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
 });
